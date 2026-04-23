1e23d593e1c8b41087a2038bdee92220a36b0f55
Leonmmcoset <testmod@qq.com>
2026-04-23 20:04:48 +0800

    删除备份

--- clks.local.bak/include/clks/kernel.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLKS_KERNEL_H
-#define CLKS_KERNEL_H
-
-void clks_kernel_main(void);
-
-#endif
--- clks.local.bak/include/clks/serial.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLKS_SERIAL_H
-#define CLKS_SERIAL_H
-
-void clks_serial_init(void);
-void clks_serial_write_char(char ch);
-void clks_serial_write(const char *text);
-
-#endif
