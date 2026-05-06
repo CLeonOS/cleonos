1913186e806f64019ff64dc90b42dd91524e522f
Leonmmcoset <testmod@qq.com>
2026-05-06 21:08:30 +0800

    修复

--- cleonos/c/include/math.h
+++ cleonos/c/include/math.h
@@ -5,4 +5,16 @@
 #define INFINITY ((double)(1.0 / 0.0))
 #endif
 
+#ifndef HUGE_VAL
+#define HUGE_VAL INFINITY
+#endif
+
+double floor(double value);
+double ceil(double value);
+double fmod(double left, double right);
+double ldexp(double value, int exponent);
+double frexp(double value, int *out_exponent);
+double sqrt(double value);
+double pow(double base, double exponent);
+
 #endif
